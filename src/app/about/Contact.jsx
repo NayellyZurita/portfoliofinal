6020a81ea7384151fc12717127cdeddd2f8bca4f
NayellyZurita <156032824+NayellyZurita@users.noreply.github.com>
2024-04-13 06:13:14 +0000

    change about

--- src/app/about/Contact.jsx
+++ src/app/about/Contact.jsx
@@ -9,17 +9,6 @@ export default function RightSide() {
         <li>
           <div >
             <svg
-              width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg "style={{ marginRight: "8px" }} className="{{ IsDarkMode ? 'fill-white' : 'fill-gray-800' }}"
-            > <path d="M11.9047 8.47089L19.3513 0.00836182H17.5873L11.1187 7.35475L5.956 0.00836182H0L7.80867 11.1185L0 19.9916H1.764L8.59067 12.2319L14.044 19.9916H20M2.40067 1.30886H5.11067L17.586 18.755H14.8753" className="fill-current"  />
-            </svg>
-            <a href="https://www.example.com" target="_blank" rel="noreferrer">
-              Follow me on X
-            </a>
-          </div>
-        </li>
-        <li>
-          <div >
-            <svg
               width="24"
               height="24"
               viewBox="0 0 24 24"
